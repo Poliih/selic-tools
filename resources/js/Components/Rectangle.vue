e49422a06149de6a8112d73fc4fb2ff2ccb41456
Poliana Rodrigues <polianarodriguesds@gmail.com>
2025-06-06 16:02:08 -0300

    :lipstick: Feat: Ajuste visual

---
+++ resources/js/Components/Rectangle.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+    value: {
+        type: [Number, String],
+        default: 0
+    },
+    previous: {
+        type: [Number, String],
+        default: 0
+    },
+    label: {
+        type: String,
+        default: 'Indicador'
+    }
+})
+
+const calcPercent = (value, previous) => {
+    const val = Number(value)
+    const prev = Number(previous)
+    if (!prev || prev === 0) return '0.00%'
+    const diff = ((val - prev) / prev) * 100
+    const prefix = diff >= 0 ? '+' : ''
+    return `${prefix}${diff.toFixed(2)}%`
+}
+</script>
+
+<template>
+    <div
+        class="w-full min-w-[200px] p-6 rounded-2xl shadow-xl drop-shadow-lg transition-all duration-300 hover:scale-[1.02] text-white bg-gradient-to-br from-[#87BFFF] to-[#3F8EFC] flex flex-col items-center text-center">
+        <div class="text-s font-bold opacity-90 text-center w-full">{{ label }}</div>
+
+        <div class="mt-2 gap-y-1">
+            <div class="text-3xl font-bold tracking-tight">
+                {{ Number(value).toLocaleString('pt-BR') }}
+            </div>
+
+            <!-- <div class="text-xs font-semibold px-2 py-1 rounded-full">
+                {{ calcPercent(value, previous) }}
+            </div> -->
+        </div>
+    </div>
+
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, computed } from 'vue'
 import axios from 'axios'
-import { Chart, controllers, registerables } from 'chart.js'
+import { Chart, registerables } from 'chart.js'
+import Rectangle from '../Components/Rectangle.vue'
 
 Chart.register(...registerables)
 
 const startDate = ref('')
 const endDate = ref('')
 const dados = ref([])
+const selicMedia = ref(null)
+const isLoading = ref(false)
 const chartRef = ref(null)
 let chartInstance = null
 
@@ -17,18 +20,19 @@ const formatarData = (data) => {
     return `${dia}/${mes}/${ano}`
 }
 
-const isLoading = ref(false)
-
 const buscarSelic = async () => {
     if (!startDate.value || !endDate.value) {
-        alert('Por favor, selecione ambas as datas.');
-        return;
+        alert('Por favor, selecione ambas as datas.')
+        return
     }
+
     if (new Date(startDate.value) > new Date(endDate.value)) {
-        alert('A data inicial não pode ser posterior à data final.');
-        return;
+        alert('A data inicial não pode ser posterior à data final.')
+        return
     }
+
     isLoading.value = true
+
     try {
         const response = await axios.get('/api/selic', {
             params: {
@@ -36,8 +40,14 @@ const buscarSelic = async () => {
                 endDate: formatarData(endDate.value),
             },
         })
-        console.log('Dados recebidos:', response.data)
+
         dados.value = response.data
+
+        if (dados.value.length > 0) {
+            const soma = dados.value.reduce((acc, item) => acc + Number(item.valor), 0)
+            selicMedia.value = soma / dados.value.length
+        }
+
         renderChart()
     } catch (error) {
         console.error(error)
@@ -54,18 +64,14 @@ const renderChart = () => {
     dados.value.forEach(item => {
         const [dia, mes, ano] = item.data.split('/')
         const chave = `${mes}/${ano.slice(-2)}`
-
-        if (!dadosAgrupados[chave]) {
-            dadosAgrupados[chave] = []
-        }
+        if (!dadosAgrupados[chave]) dadosAgrupados[chave] = []
         dadosAgrupados[chave].push(Number(item.valor))
     })
 
     const labels = Object.keys(dadosAgrupados)
     const valores = labels.map(chave => {
         const soma = dadosAgrupados[chave].reduce((a, b) => a + b, 0)
-        const media = soma / dadosAgrupados[chave].length
-        return Number(media.toFixed(7))
+        return Number((soma / dadosAgrupados[chave].length).toFixed(7))
     })
 
     if (labels.length < 2) {
@@ -73,9 +79,7 @@ const renderChart = () => {
         return
     }
 
-    if (chartInstance) {
-        chartInstance.destroy()
-    }
+    if (chartInstance) chartInstance.destroy()
 
     chartInstance = new Chart(chartRef.value, {
         type: 'line',
@@ -84,7 +88,7 @@ const renderChart = () => {
             datasets: [{
                 label: 'Taxa Selic Média (%) por Mês',
                 data: valores,
-                borderColor: 'rgb(75, 192, 192)',
+                borderColor: '#2667FF',
                 tension: 0.5,
                 fill: false,
             }]
@@ -98,7 +102,7 @@ const renderChart = () => {
             scales: {
                 y: {
                     ticks: {
-                        callback: (value) => value.toFixed(3) + '%'
+                        callback: value => value.toFixed(3) + '%'
                     }
                 },
                 x: {
@@ -110,30 +114,61 @@ const renderChart = () => {
         }
     })
 }
+
+const entreDatas = computed(() => {
+    if (!startDate.value || !endDate.value) return 0
+
+    const start = new Date(startDate.value)
+    const end = new Date(endDate.value)
+    if (end < start) return 0
+
+    const diffTime = Math.abs(end - start)
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1
+})
+
+const ontem = computed(() => {
+    const hoje = new Date()
+    const ontemDate = new Date(hoje)
+    ontemDate.setDate(hoje.getDate() - 1)
+    return ontemDate.toISOString().substring(0, 10)
+})
+
+watch([startDate, endDate], () => {
+    console.log('Dias entre datas:', entreDatas.value)
+})
 </script>
 
 <template>
-    <div class="p-6 max-w-5xl mx-auto">
-        <h1 class="text-3xl font-bold mb-6">Dashboard SELIC</h1>
+    <div class="p-6 max-w-5xl mx-auto m-10">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-10 px-4">
+            <Rectangle :value="entreDatas" label="Quantidade de dias" />
+            <Rectangle :value="selicMedia" label="Média por período" />
+            <Rectangle :value="dados.length" label="Total de registros" />
+        </div>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
             <div>
                 <label class="block text-sm font-medium text-gray-700">Data Início</label>
-                <input type="date" v-model="startDate" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                <input type="date" v-model="startDate"
+                    class="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none" />
             </div>
             <div>
                 <label class="block text-sm font-medium text-gray-700">Data Final</label>
-                <input type="date" v-model="endDate" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+                <input type="date" v-model="endDate"
+                    class="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none" />
             </div>
             <div class="flex items-end gap-4">
-                <button @click="buscarSelic"
-                    class="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md shadow disabled:opacity-50"
-                    :disabled="isLoading">
+                <button @click="buscarSelic" class="w-full py-2 px-4 rounded-md shadow disabled:opacity-50
+                text-white text-center flex flex-col items-center
+                bg-gradient-to-br from-[#87BFFF] to-[#3F8EFC]
+                hover:brightness-110" :disabled="isLoading">
                     {{ isLoading ? 'Buscando...' : 'Buscar' }}
                 </button>
                 <div class="mt-6" v-if="dados.length">
                     <a :href="`/api/exportar?startDate=${formatarData(startDate)}&endDate=${formatarData(endDate)}`"
-                        class="inline-flex items-center bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md shadow">
+                        class="inline-flex items-center text-white px-4 py-2 rounded-md shadow
+                        bg-gradient-to-br from-[#7EDC98] to-[#28B463]
+                        hover:brightness-110 transition duration-300 ease-in-out">
                         Exportar
                     </a>
                 </div>
@@ -146,10 +181,10 @@ const renderChart = () => {
 
         <div v-if="dados.length" class="overflow-x-auto mt-4 border rounded-md shadow-sm">
             <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-100">
+                <thead class="bg-[#2667FF]">
                     <tr>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Data</th>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Valor (%)</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase">Data</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase">Valor (%)</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
